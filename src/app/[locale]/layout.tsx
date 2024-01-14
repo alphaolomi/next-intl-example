adc90626496974f01b22e13b41fbc589216ab470
Alpha Olomi <alphaolomi@gmail.com>
2024-01-14 22:19:07 +0300

    with auth

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,43 +1,23 @@
-import clsx from 'clsx';
-import {Inter} from 'next/font/google';
-import {getTranslations, unstable_setRequestLocale} from 'next-intl/server';
+import {NextIntlClientProvider, useMessages} from 'next-intl';
 import {ReactNode} from 'react';
-import Navigation from 'components/Navigation';
-import {locales} from '../../config';
-
-const inter = Inter({subsets: ['latin']});
 
 type Props = {
   children: ReactNode;
   params: {locale: string};
 };
 
-export function generateStaticParams() {
-  return locales.map((locale) => ({locale}));
-}
-
-export async function generateMetadata({
-  params: {locale}
-}: Omit<Props, 'children'>) {
-  const t = await getTranslations({locale, namespace: 'LocaleLayout'});
-
-  return {
-    title: t('title')
-  };
-}
-
-export default async function LocaleLayout({
-  children,
-  params: {locale}
-}: Props) {
-  // Enable static rendering
-  unstable_setRequestLocale(locale);
+export default function LocaleLayout({children, params: {locale}}: Props) {
+  const messages = useMessages();
 
   return (
-    <html className="h-full" lang={locale}>
-      <body className={clsx(inter.className, 'flex h-full flex-col')}>
-        <Navigation />
-        {children}
+    <html lang={locale}>
+      <head>
+        <title>next-intl & next-auth</title>
+      </head>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
       </body>
     </html>
   );
